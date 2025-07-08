592d3c0f05682c556f7d7d8173eaaf0bc84e3a09
basavesh48 <basavesh.m@s.amity.edu>
2025-07-08 22:11:18 +0530

    added fruits avaliable

--- fruit_bill.py
+++ fruit_bill.py
@@ -11,3 +11,12 @@ for fruit, price in fruit_prices.items():
     print(f"{fruit.title()}: ₹{price}/kg")
 
 total_bill = 0
+while True:
+    fruit = input("\nEnter the name of the fruit (or type 'done' to finish): ").lower()
+
+    if fruit == 'done':
+        break
+    elif fruit not in fruit_prices:
+        print("Sorry, we don't have that fruit.")
+        continue
+
